2b780e0fc99c68e27f115ce435adb190657eabae
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2021-10-27 17:06:27 +0200

    methode et props

--- src/App.js
+++ src/App.js
@@ -3,29 +3,57 @@ import React from "react"
 import "./styles/global.css"
 import Box from "./components/Box"
 
-// const tempMin = -20
-// const tempMax = 40
-// const heartMin = 80
-// const heartMax = 180
-// const stepsMin = 0
-// const stepsMax = 50000
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax = 180
+const stepsMin = 0
+const stepsMax = 50000
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      water: 1.5,
+      heart: 120,
+      temperature: -10,
+      steps: 3000
+    }
+  }
+
+  onHeartChange = event => {
+    this.setState({ heart: this.state.heart = event.target.value })
+  }
+  
+  onStepsChange = event => {
+    this.setState({ steps: this.state.steps = event.target.value })
+  }
+  
+  onTemperatureChange = event => {
+    this.setState({ teperature: this.state.temperature = event.target.value })
+  }
+
+  calculateWater = () => {
+    
+  }
+  
+  
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
           {/* Water */}
-          <Box icon="local_drink" color="#3A85FF" value="1.5" unit="L" />
+          <Box status="water" icon="local_drink" color="#3A85FF" value={this.state.water} unit="L" />
 
           {/* Steps */}
-          <Box icon="directions_walk" color="black" value="3000" unit="steps" />
+          <Box status="steps" icon="directions_walk" color="black" value={this.state.steps} unit="steps" min={stepsMin} max={stepsMax} onChange={this.onStepsChange}/>
           
           {/* Heart */}
-          <Box icon="favorite" color="red" value="120" unit="bpm" />
+          <Box status="heart" icon="favorite" color="red" value={this.state.heart} unit="bpm" min={heartMin} max={heartMax} onChange={this.onHeartChange}/>
           
           {/* Temperature */}
-          <Box icon="wb_sunny" color="yellow" value="-10" unit="°C" />
+          <Box status="temperature" icon="wb_sunny" color="yellow" value={this.state.temperature} unit="°C" min={tempMin} max={tempMax} onChange={this.onTemperatureChange}/>
         </div>
       </div>
     )
--- src/components/Box.js
+++ src/components/Box.js
@@ -2,14 +2,16 @@ import React from "react"
 
 class Box extends React.Component {
   render() {
-    console.log(this.props)
+
+    const { status, value, icon, color, unit, max, min, onChange } = this.props
 
     return (
       <div className="box col-sm-3 col-6">
-        <span className="material-icons" style={{ fontSize: "100px", color: this.props.color }}>
-          {this.props.icon}
+        <span className="material-icons" style={{ fontSize: "100px", color: color }}>
+          {icon}
         </span>
-        <p>{this.props.value}{this.props.unit}</p>
+        <p>{value}{unit}</p>
+        {status !== "water" ? <input type="range" min={min} max={max} value={value} onChange={onChange}/> : ""}
       </div>
     )
   }
